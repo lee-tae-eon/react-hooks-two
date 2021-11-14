c810cb8077f7fda7223ffa2650abc0fd279cabf9
lee-tae-eon <mktears0609@gmail.com>
2021-11-14 21:39:23 +0900

    19.9

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
+const initialState = {
+  toDos: [],
+};
+
+const ADD = "add";
+const DEL = "del";
 
 function reducer(state, action) {
   switch (action.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
+    case ADD:
+      return {
+        toDos: [...state.toDos, { text: action.payLoad, id: uuidv4() }],
+      };
+    case DEL:
+      return {};
     default:
       throw new Error();
   }
 }
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewTodo] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD, payLoad: newToDo });
+    setNewTodo("");
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewTodo(value);
+  };
 
   return (
     <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: INCREMENT })}>더하기</button>
-      <button onClick={() => dispatch({ type: DECREMENT })}>뺴기</button>
+      <h1>ADD To Dos</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Write to do"
+          value={newToDo}
+          onChange={onChange}
+        />
+      </form>
+      <ul>
+        <h2>To Dos</h2>
+        {state.toDos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button>❌</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
