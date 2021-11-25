18eae468ce7fc4d83d3a5f1e3f4e49ff2df8befa
lee-tae-eon <mktears0609@gmail.com>
2021-11-25 22:04:11 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 import Add from "./Add";
 import List from "./List";
 
-
 const App = () => {
-
   const [newToDo, setNewTodo] = useState("");
 
   return (
@@ -14,41 +12,45 @@ const App = () => {
         <h1>text</h1>
         <h2>hello</h2>
         <h2>To Dos</h2>
-        {state.toDos.map((todo) => (
-          <li key={todo.id}>
-            <span>{todo.text}</span>
-            <span onClick={() => dispatch({ type: DEL, payLoad: todo.id })}>
-              ❌
-            </span>
-            <span
-              onClick={() => dispatch({ type: COMPLETE, payLoad: todo.id })}
-            >
-              ✅
-            </span>
-          </li>
-        ))}
-      </ul>
-      <ul>
-        {state.completed.length !== 0 && (
-          <>
-            <h2>Completed</h2>
-            {state.completed.map((todo) => (
-              <li key={todo.id}>
-                <span>{todo.text}</span>
-                <span onClick={() => dispatch({ type: DEL, payLoad: todo.id })}>
-                  ❌
-                </span>
-                <span
-                  onClick={() =>
-                    dispatch({ type: UNCOMPLETE, payLoad: todo.id })
-                  }
-                >
-                  😂👍🏻
-                </span>
-              </li>
-            ))}
-          </>
-        )}
+        <ul>
+          {state.toDos.map((todo) => (
+            <li key={todo.id}>
+              <span>{todo.text}</span>
+              <span onClick={() => dispatch({ type: DEL, payLoad: todo.id })}>
+                ❌
+              </span>
+              <span
+                onClick={() => dispatch({ type: COMPLETE, payLoad: todo.id })}
+              >
+                ✅
+              </span>
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {state.completed.length !== 0 && (
+            <>
+              <h2>Completed</h2>
+              {state.completed.map((todo) => (
+                <li key={todo.id}>
+                  <span>{todo.text}</span>
+                  <span
+                    onClick={() => dispatch({ type: DEL, payLoad: todo.id })}
+                  >
+                    ❌
+                  </span>
+                  <span
+                    onClick={() =>
+                      dispatch({ type: UNCOMPLETE, payLoad: todo.id })
+                    }
+                  >
+                    😂👍🏻
+                  </span>
+                </li>
+              ))}
+            </>
+          )}
+        </ul>
       </List>
     </>
   );
